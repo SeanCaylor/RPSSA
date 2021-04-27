cb1603e02f9384fbda5ab0d830ed6b27b35ad4a3
SeanCaylor <seanpcaylor@gmail.com>
2021-04-26 18:11:31 -0700

    JavaScript improved, 70% functional

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//GOOD confirmed multiple times over.
+//Logic engine for the game
 function determineVictor(userChoice, comChoice){
-    console.log(win, lose)
+    console.log("wins and losses are: ", win, lose)
+    console.log("the choices were user: ", userChoice, "and com:", comChoice)
     var result = []
     //From here on 1:Rock, 2:Lizard, 3:Alien, 4:Scissors, 5: Paper
     if (userChoice === comChoice){
@@ -85,15 +88,15 @@ function determineVictor(userChoice, comChoice){
             return result;
         }
         else if (comChoice === 2){
-            result = ["lose", 7];
+            result = ["lose", 8];
             return result;
         }
         else if (comChoice === 3){
-            result ["win", 3];
+            result = ["win", 3];
             return result;
         }
         else if (comChoice === 4){
-            result ["lose", 11];
+            result = ["lose", 11];
             return result;
         }
     }
@@ -101,7 +104,7 @@ function determineVictor(userChoice, comChoice){
 
 function gameOn(choice){
     console.log("but are we getting here?")
-    var comPlay = rdmDigit(1,5);
+    var comPlay = rdmDigit();
     var gameRound = determineVictor(choice, comPlay);
 
     if (gameRound[0] == "win"){
@@ -122,43 +125,26 @@ function gameOn(choice){
         console.log("are we getting here? ln122")
 }
 
-function gameOverCheck(w, l){
-    console.log("we getting here?ln126")
-    var actionsClss = document.getElementsByClassName("actions");
-    if (w === 2 || l === 2){
-        gameOver = true;
-        if (w > l){
-            viewScreenChango("WinScreen");
-        }
-        else if (w < l){
-            viewScreenChango("LoseScreen");
-        }
-        updateText();
-        actionsClss.class = "gOver";
-    }
-    return;
-}
-
 function powerOff(){
     uRock.onclick = "";
     uSnake.onclick = "";
     uAlien.onclick = "";
     uScissors.onclick = "";
     uPaper.onclick = "";
-    console.log("are we getting here? ln210")
 }
 
-function rdmDigit(min, max){
-		return Math.floor(Math.random() * (max - min) + min);
+//good
+function rdmDigit(){
+	return Math.floor(Math.random() * (5 - 1 + 1) + 1);
 }
 
-function updateText(lotNum) {
+function updateText(lotNum, indx) {
     var ln1 = document.getElementById("actDesc"), ln2 = document.getElementById("advCount");
-    var dict2Index = 0
+    var dict2Index = indx
 
     var gameDict1 = {
         1: "A tie!",
-        2: "Rock crushes lizard!",
+        2: "Rock crushes snake!",
         3: "The paper disproves aliens!",
         4: "The rock, as it has always done, crushes the scissors!",
         5: "Paper covers rock!",
@@ -178,26 +164,12 @@ function updateText(lotNum) {
         4: "Click Home to reload or MyProfile to go back",
     }       
 
-    if (win > lose){
-        dict2Index = 1;
-        console.log("are we getting here? ln149")
-    }
-    else if (lose > win){
-        dict2Index = 2;
-        console.log("are we getting here? ln153")
-    }
-    else if (win === lose){
-        dict2Index = 3;
-        console.log("are we getting here? ln157")
-    }
-
-    if (gameOver == true){
-        lotNum = 12, gameDict2 = 4;
-    }
-    console.log(gameDict1.lotNum);
-    ln1.innerHTML = gameDict1.lotNum;
-    console.log(gameDict2.dict2Index);
-    ln2.innerHTML = gameDict2.dict2Index;
+    ln1.innerHTML = gameDict1[lotNum];
+    ln2.innerHTML = gameDict2[dict2Index];
+    console.log(lotNum)
+    console.log(dict2Index)
+    console.log(gameDict1[lotNum])
+    console.log(gameDict2[lotNum])
 }
 
 function viewScreenChango(frame){
@@ -217,5 +189,4 @@ var win = 0;
 var lose = 0;
 var gameOver = false;
 
-setInterval(gameOverCheck(), 5000, win, lose);
 
